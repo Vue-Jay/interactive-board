b710b3a1bbe5a0e13e013f792a001595ae910e57
Vue-Jay <yahaus04@gmail.com>
2026-09-25 01:58:04 +0500

    v111: complete mobile and tablet responsive interface

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="onlinerepetitor-shell-v110";
+const CACHE="onlinerepetitor-shell-v111";
 const SHELL=["/","/index.html","/manifest.webmanifest","/favicon.svg","/pwa-icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));});
 self.addEventListener("message",event=>{if(event.data?.type==="SKIP_WAITING")self.skipWaiting()});
